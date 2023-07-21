804266a471d150a6f237aa76f52c05f8ad51c6ed
GTeru <gmteruya@hotmail.com>
2023-07-21 01:03:09 -0300

    fix: webpack configuration for hotreload on container

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, context) => {
+    config.watchOptions = {
+      poll: 1000,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
